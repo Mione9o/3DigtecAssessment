514eb53f96f06bb732d0be843973720c0aca07b1
19062hm <50893455+19062hm@users.noreply.github.com>
2023-08-14 02:32:03 +0000

    Week 4 Summary (Term 3)
    7th August - Begin GUI Added In, beginTutorial() and skipTutorial() Functions Created For Tutorial Start
    8th August - Exit Button Created (Also Styled & Positioned), Exit GUI Added & Positioned (With X Button Added In Case Of Misclick), exitGame(), quitGame() & returnGame() Functions Created To Return Player To Start
    9th August - Rocket Ship & Rocket Ship Launching Images Added (And Positioned), movement() Function Created For Game Controls
    11th August - Fixed movement() Function Bug (Found Code That Actually Works), Left & Right Key Controls Added (But Contains Size Bug), Up & Down Buttons Yet To Be Added

--- script.js
+++ script.js
@@ -15,17 +15,142 @@ function loadIn() {
   let load = document.querySelector('.load'),
     button = document.querySelector('.playButton'),
     banner = document.querySelector('.banner'),
-    title = document.querySelector('.title');
+    nav = document.querySelector('.menu'),
+    title = document.querySelector('.title'),
+    begin = document.querySelector('.begin'),
+    exit = document.querySelector('.exit'),
+    rocket = document.querySelector('.rocket');
   
   button.classList.add('hide');
   banner.classList.add('hide');
   title.classList.add('hide'); 
-  load.classList.add('show');
+  nav.classList.add('hide');
+  load.classList.add('see');
   setTimeout(loadOut, 5000);
   
   function loadOut() {
-    load.classList.remove('show');
+    load.classList.remove('see');
+    begin.classList.add('show');
+    exit.classList.add('show');
+    rocket.classList.add('show');
+    
   }
 };
 
+function beginTutorial() {
+  let begin = document.querySelector('.begin');
+
+  begin.classList.remove('show');
+}
+
+function skipTutorial() {
+  let begin = document.querySelector('.begin'),
+    load = document.querySelector('.load');
+
+  begin.classList.remove('show');
+  load.classList.add('see');
+  setTimeout(endSkip, 1500);
+
+  function endSkip() {
+    load.classList.remove('see');
+  }
+};
+
+function exitGame() {
+  let quit = document.querySelector('.exiting'),
+    begin = document.querySelector('.begin');
+
+  quit.classList.add('show');
+  begin.classList.remove('show');
+}
+
+function quitGame() {
+  let quit = document.querySelector('.exiting'),
+    load = document.querySelector('.load'),
+    button = document.querySelector('.playButton'),
+    banner = document.querySelector('.banner'),
+    nav = document.querySelector('.menu'),
+    title = document.querySelector('.title'),
+    exit = document.querySelector('.exit'),
+    rocket = document.querySelector('.rocket');
+
+  load.classList.add('see');
+  setTimeout(defaultScreen, 1000);
+
+  function defaultScreen() {
+    quit.classList.remove('show');
+    button.classList.remove('hide');
+    banner.classList.remove('hide');
+    nav.classList.remove('hide');
+    title.classList.remove('hide');
+    exit.classList.remove('show');
+    rocket.classList.remove('show');
+  }
+  setTimeout(endQuit, 4000);
+
+  function endQuit() {
+    load.classList.remove('see');
+  }
+}
+
+function returnGame() {
+  let quit = document.querySelector('.exiting'),
+    begin = document.querySelector('.begin');
+
+  quit.classList.remove('show');
+  begin.classList.add('show');
+}
+
+function movement() {
+  document.onkeydown = detectKey;
+
+  function detectKey(e) {
+    var posLeft = document.getElementById("life").offsetLeft;
+    var posTop = document.getElementById("rocket").offsetTop;
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+      /* Up Arrow */
+      document.getElementById("rocket").style.transform = "rotate(0deg)";
+      document.getElementById("life").style.marginTop = (posTop-10)+"px";
+    } else if (e.keyCode == '40') {
+      /* Down Arrow */
+      document.getElementById("rocket").style.transform = "rotate(180deg)";
+      document.getElementById("life").style.marginTop = (posTop+10)+"px";
+    } else if (e.keyCode == '37') {
+      /* Left Arrow */
+      document.getElementById("rocket").style.transform = "rotate(270deg)";
+      document.getElementById("life").style.marginLeft = (posLeft-10)+"px";
+    } else if (e.keyCode == '39') {
+      /* Right Arrow */
+      document.getElementById("rocket").style.transform = "rotate(90deg)";
+      document.getElementById("life").style.marginLeft = (posLeft+10)+"px";
+    }
+  }
+}
+
+function moving() {
+  let circle = document.querySelector(".rocket"),
+    moveBy = 20;
+  
+  window.addEventListener("keyup", (e) => {
+    switch (e.key) {
+      case "ArrowLeft":
+        circle.style.left = parseInt(circle.style.left) - moveBy + "px";
+        break;
+      case "ArrowRight":
+        circle.style.left = parseInt(circle.style.left) + moveBy + "px";
+        break;
+      case "ArrowUp":
+        circle.style.top = parseInt(circle.style.top) - moveBy + "px";
+        break;
+      case "ArrowDown":
+        circle.style.top = parseInt(circle.style.top) + moveBy + "px";
+        break;
+    }
+  });
+}
+
 navigation();
+moving();
